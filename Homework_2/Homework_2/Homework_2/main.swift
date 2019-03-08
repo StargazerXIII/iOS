7774db161501fbe9c02ff029e7073468632cc35b
Stargazer <mr.kuznetsov@protonmail.ch>
2019-03-08 23:10:57 +0300

    Задание 1

---
+++ Homework_2/Homework_2/Homework_2/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Homework_2
+//
+//  Created by VADIM KUZNETSOV on 08/03/2019.
+//  Copyright © 2019 VADIM KUZNETSOV. All rights reserved.
+//
+
+import Foundation
+
+// Задание 1: Написать функцию, которая определяет, четное число или нет.
+
+func number(_a: Int) -> Bool {
+    return _a%2==0
+}
+print(number(_a: ))
+
+// Задание 2: Написать функцию, которая определяет, делится ли число без остатка на 3.
+
+
